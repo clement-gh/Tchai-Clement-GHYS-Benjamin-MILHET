620b16aa0eaa1d895cdb11ee5b0e965814a53792
milhet <benjamin.milhet@outlook.fr>
2023-11-13 16:15:28 +0100

    sort getTransaction

--- TCHAI.py
+++ TCHAI.py
@@ -27,10 +27,10 @@ def getTransactions():
     # curl -X GET  http://127.0.0.1:5000/getTransactions
 
     liste_res = []
-    tmp = rTransaction.keys("transaction.*")
+    tmp = rTransaction.keys("transaction.*.*")
+    tmp.sort()
     for i in range(len(tmp)):
-        liste_res.append(tmp[i])
-        #liste_res.append(rTransaction.get(tmp[i]))
+        liste_res.append(rTransaction.get(tmp[i]))
 
     return liste_res
 
