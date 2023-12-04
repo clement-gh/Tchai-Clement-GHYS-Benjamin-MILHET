5435248cc70d0f5c247f0885bcd6b6f1cffdc62e
Clément <clement.cg15@gmail.com>
2023-12-04 11:11:27 +0100

    post transaction

--- TCHAI.py
+++ TCHAI.py
@@ -1,3 +1,5 @@
+import calendar
+import time
 from flask import Flask, request
 import sys
 import redis
@@ -90,7 +92,6 @@ def charger_donnees():
     rUser.set("solde.Clement", "200")
 
 
-    # charger tweets
     rTransaction.set("transaction.1.donneur", "Benjamin")
     rTransaction.set("transaction.1.receveur", "Clement")
     rTransaction.set("transaction.1.valeur", "100")
@@ -106,20 +107,31 @@ def enregistrer_transaction():
 
 
     #  curl -X POST -H "Content-Type: application/json; charset=utf-8" --data "{\"donneur\":\"Benjamin\", \"receveur\":\"Clement\", \"valeur\":\"100\"}" http://localhost:5000/enregisterTransaction
+    time_stamp = calendar.timegm(time.gmtime())
 
     data = request.get_json()
     donneur = data.get("donneur")
     receveur = data.get("receveur")
     valeur = data.get('valeur')
- 
+    
 
+    liste_transaction_donneur= json.loads(rUser.get("transaction." + donneur))
+    liste_transactin_receveur= json.loads(rUser.get("transaction." + receveur))
+    print(donneur + " " + receveur )
+
+    #on ajoute la transaction au donneur
+    liste_transaction_donneur.append(time_stamp)   
+    rUser.set("transaction." + donneur, json.dumps(liste_transaction_donneur))
+    
+    #on ajoute la transaction au receveur
+    liste_transactin_receveur.append(time_stamp)
+    rUser.set("transaction." + receveur, json.dumps(liste_transactin_receveur))
 
     #on ajoute la transaction 
-    rTransaction.set("transaction." + str(rTransaction.dbsize()+1) + ".donneur", donneur)
-    rTransaction.set("transaction." + str(rTransaction.dbsize()+1) + ".receveur", receveur)
-    rTransaction.set("transaction." + str(rTransaction.dbsize() +1) + ".valeur", valeur)
-    
-    
+    rTransaction.set("transaction." + str(time_stamp) + ".donneur", donneur)
+    rTransaction.set("transaction." + str(time_stamp) + ".receveur", receveur)
+    rTransaction.set("transaction." + str(time_stamp) + ".valeur", valeur)
+  
     #mise a jour du solde du donneur
     solde_donneur = int(rUser.get("solde." + donneur))
     solde_donneur = solde_donneur - int(valeur)
@@ -149,21 +161,11 @@ def get_solde():
 
     
 
-
-
-
-
-
-
-
-    
-
-    
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "check_syntax":
             print("Build [ OK ]")
+            charger_donnees()
             exit(0)
         else:
             print("Passed argument not supported ! Supported argument : check_syntax")
